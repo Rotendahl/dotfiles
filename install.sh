5808ad82005678897f55e323022cb2adbfcbc24f
Benjamin <benjamin@rotendahl.dk>
2023-07-04 23:13:33 +0200

    WD functions

--- install.sh
+++ install.sh
@@ -3,9 +3,14 @@
 
 # Get Completions
 wget -O fish/completions/docker.fish https://github.com/docker/cli/raw/master/contrib/completion/fish/docker.fish
+wget -O fish/completions/wd.fish https://github.com/Rotendahl/plugin-wd/raw/master/completions/wd.fish
 helm completion fish > fish/completions/helm.fish
 kubectl completion fish > fish/completions/kubectl.fish
 gh completion -s fish > fish/completions/gh.fish
+
+
+# Get functions
+wget -O fish/functions/wd.fish https://raw.githubusercontent.com/Rotendahl/plugin-wd/master/functions/wd.fish
 
 # Fish
 ln -s fish ~/.config/fish
