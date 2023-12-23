92f86bb208da3fae3252545a2987a5c55c75287e
Benjamin <benjamin@rotendahl.dk>
2023-12-23 19:47:48 +0100

    Also restart depoys

--- fish/functions/resetDeployments.fish
+++ fish/functions/resetDeployments.fish
@@ -3,6 +3,7 @@ function resetDeployments
     set apps $(kubectl get deployments.apps -o name | sed 's/deployment.apps\///g')
     for app in $apps
         kubectl set image deployment/$app $app=monthioacr.azurecr.io/$app:latest
+        kubectl rollout restart deployment $app
         if test $status -eq 0
             echo "$app set to latest"
         else
