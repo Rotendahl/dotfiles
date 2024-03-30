f09ded00e5d4f25549009198d6b4020e50318ec6
Benjamin <benjamin@rotendahl.dk>
2024-03-30 11:36:17 +0100

    Connect telepresence on vpn connection

--- fish/functions/vonthio.fish
+++ fish/functions/vonthio.fish
@@ -1,5 +1,7 @@
 # Enables the VPN connection in macOs prefs called Vonthio
 function vonthio
+    echo "Connecting telepresence..."
+    telepresence connect
     if test $(networksetup -showpppoestatus Vonthio) != connected
         networksetup -connectpppoeservice Vonthio
         confetti
