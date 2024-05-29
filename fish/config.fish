3163814e96a41dc49b733bee36f114117e631254
Benjamin <benjamin@rotendahl.dk>
2024-05-29 10:55:33 +0200

    Use new built in theme switcher in iterm2

--- fish/config.fish
+++ fish/config.fish
@@ -33,10 +33,3 @@ abbr --add tele-enricher telepresence intercept transaction-enrichment-app --por
 
 ## Acitvate pyenv
 pyenv init - | source
-
-## Set colorscheme
-if test $(dark-mode status) = off
-    it2setcolor preset 'Solarized Light'
-else
-    it2setcolor preset Dracula
-end
