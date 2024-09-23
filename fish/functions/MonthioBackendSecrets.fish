3f92f37f7bc15d1024126e59820ab9bfe65305c5
Benjamin <benjamin@rotendahl.dk>
2024-09-23 10:50:38 +0200

    Update secret runner

--- fish/functions/MonthioBackendSecrets.fish
+++ fish/functions/MonthioBackendSecrets.fish
@@ -1,6 +1,14 @@
 # Inject env vars from 1Password into the current shell
 function MonthioBackendSecrets
+    set envFileName "bro-op.env"
+    if test ! -e $envFileName
+        printf "%s\n" "No $envFileName file found. Please create one with the following format:
+        AzureKeyVault__ClientId=\"op://some-val\"
+        AzureKeyVault__ClientSecret=\"op://some-other-val\"
+        "
+        return 1
+    end
+
     vonthio
-    set envFile /Users/rotendahl/.envs/monthio-dotnet.env
-    op --account Monthio run --no-masking --env-file=$envFile -- $argv
+    op --account Monthio run --no-masking --env-file=$envFileName -- $argv
 end
