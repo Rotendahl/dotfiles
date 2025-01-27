2009ca6634f2813f32878132c2bf9db293bb152d
Benjamin <benjamin@rotendahl.dk>
2025-01-27 09:15:55 +0100

    Remove telepresence from vonthio

--- fish/functions/vonthio.fish
+++ fish/functions/vonthio.fish
@@ -1,7 +1,5 @@
 # Enables the VPN connection in macOs prefs called Vonthio
 function vonthio
-    echo "Connecting telepresence..."
-    telepresence connect 
     loft wakeup vcluster brotual
     if test $(networksetup -showpppoestatus Vonthio) != connected
         networksetup -connectpppoeservice Vonthio
