23540a8c96bf5cab343ccf8e4ac9f53c5a7db544
Benjamin <benjamin@rotendahl.dk>
2025-09-09 09:55:40 +0200

    Remove wakeup command

--- fish/functions/vonthio.fish
+++ fish/functions/vonthio.fish
@@ -1,6 +1,5 @@
 # Enables the VPN connection in macOs prefs called Vonthio
 function vonthio
-    loft wakeup vcluster brotual
     if test $(networksetup -showpppoestatus Vonthio) != connected
         networksetup -connectpppoeservice Vonthio
         confetti
